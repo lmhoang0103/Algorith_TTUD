c7c88ca45463d9871d694823d9f2ba1c2396f1ca
Le Minh Hoang <lmhoang0103@gmail.com>
2022-06-30 17:06:07 +0700

    prepare

---
+++ Midterm_2/bai1.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX = 1000001;
+
+int n;
+int a[MAX];
+int s[MAX];
+int ans;
+
+void input()
+{
+  cin >> n;
+  for (int i = 0; i < n; i++)
+  {
+    cin >> a[i];
+    // cout << a[i];
+  }
+}
+
+void solve()
+{
+  s[0] = 1;
+  ans = 0;
+  for (int i = 1; i < n - 1; i++)
+  {
+    if (a[i] < a[i - 1])
+    {
+      s[i] = s[i - 1] + 1;
+    }
+    else
+    {
+      s[i] = 1;
+    }
+
+    ans = max(ans, s[i]);
+  }
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  input();
+  solve();
+  cout << ans;
+  return 0;
+}
